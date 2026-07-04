11e27f1947a5ed2f4c81f9d878c4f44c8abf1635
Dev.Kasra <ksrybharwnd@gmail.com>
2026-07-04 16:51:37 +0330

    finishing the main hero and most watched section

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -2,6 +2,7 @@ import MovieCarousel from "../../components/movie/MovieCarousel";
 import { movieService } from "../../services/movie.service";
 
 import MainHero from "./components/MainHero";
+import MostWatched from "./components/MostWatched";
 
 import GenreSec from "./components/genreSec";
 
@@ -19,6 +20,7 @@ const HomePage = () => {
           fetchMovies={movieService.getNowPlaying}
           title={"Latest Released"}
         />
+        <MostWatched/>
         <MovieCarousel
           fetchMovies={movieService.getTrending}
           title={"Trending movies & shows"}
